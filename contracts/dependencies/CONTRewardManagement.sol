8e5ddbb39ae8a1212a8f112c0a93ef3f2bb33e64
lowie1803 <dung.totuan01@gmail.com>
2022-04-29 10:11:55 +0700

    fix: count breakeven contract flow changed to save admin fees

--- contracts/dependencies/CONTRewardManagement.sol
+++ contracts/dependencies/CONTRewardManagement.sol
@@ -52,7 +52,7 @@ contract CONTRewardManagement is Initializable {
 
     // ----- Admin Dashboard Variables -----
     mapping(ContType => uint256) private _totalContsPerType;
-    mapping(ContType => uint256) private _breakevenContsPerType;
+    mapping(address => mapping(uint256 => bool)) public _brokeevenContract;
 
     // ----- Events -----
     event BreakevenChanged(ContType _cType, uint256 delta);
@@ -91,11 +91,19 @@ contract CONTRewardManagement is Initializable {
     }
 
     // ----- External WRITE functions -----
+    /**
+        @notice change admin of contract
+        @param newAdmin address of newAdmin
+    */
     function setAdmin(address newAdmin) external onlyAuthorities {
         require(newAdmin != address(0), "zero address");
         admin0XB = newAdmin;
     }
 
+    /**
+        @notice set 0xB token Address
+        @param token_ new address of 0xB
+    */
     function setToken(address token_) external onlyAuthorities {
         require(token_ != address(0), "zero address");
         token = token_;
@@ -143,6 +151,13 @@ contract CONTRewardManagement is Initializable {
         uint256 currentTstamp = block.timestamp;
         uint256 rewardCont = contRewardInInterval(cont, cont.lastUpdateTime, currentTstamp);
         cont.lastUpdateTime = currentTstamp;
+
+        if (!_brokeevenContract[account][_contIndex]) {
+            if (cont.buyPrice <= contRewardInInterval(cont, cont.creationTime, block.timestamp)) {
+                _brokeevenContract[account][_contIndex] = true;
+                emit BreakevenChanged(cont.cType, 1);
+            }
+        }
         return (rewardCont, cont.cType);
     }
 
@@ -165,31 +180,49 @@ contract CONTRewardManagement is Initializable {
         ContEntity storage _cont;
         uint256 rewardsTotal = 0;
         uint256[3] memory typeTotal = [rewardsTotal, rewardsTotal, rewardsTotal];
-        uint256 currentTstamp = block.timestamp;
-        uint256 latestCashout = 0;
-        for (uint256 i = 0; i < contsCount; i++) {
-            uint256 lastUpd = conts[i].lastUpdateTime;
-            if (lastUpd > latestCashout) {
-                latestCashout = lastUpd;
-            }
-        }
 
-        require(claimable(latestCashout), "CASHOUT ERROR: You have to wait before claiming all conts.");
+        uint8[3] memory newBreakeven = [0, 0, 0];
 
         for (uint256 i = 0; i < contsCount; i++) {
             _cont = conts[i];
-            uint256 contReward = contRewardInInterval(_cont, _cont.lastUpdateTime, currentTstamp);
+            uint256 contReward = contRewardInInterval(_cont, _cont.lastUpdateTime, block.timestamp);
             rewardsTotal += contReward;
             typeTotal[uint8(_cont.cType)] += contReward;
-            _cont.lastUpdateTime = currentTstamp;
+            _cont.lastUpdateTime = block.timestamp;
+
+            if (
+                !_brokeevenContract[account][i] &&
+                _cont.buyPrice <= contRewardInInterval(_cont, _cont.creationTime, block.timestamp)
+            ) {
+                _brokeevenContract[account][i] = true;
+                uint8 ct = uint8(_cont.cType);
+                newBreakeven[ct] = newBreakeven[ct] + 1;
+            }
+        }
+        for (uint8 ct = 0; ct < 3; ct++) {
+            if (newBreakeven[ct] > 0) {
+                emit BreakevenChanged(ContType(ct), newBreakeven[ct]);
+            }
         }
         return (rewardsTotal, typeTotal[0], typeTotal[1], typeTotal[2]);
     }
 
+    /**
+        @notice change contract price of one type
+        @param _cType contract type to change price
+        @param newPrice new price per contract (0xB)
+    */
     function _changeContPrice(ContType _cType, uint256 newPrice) external onlyAuthorities {
         contPrice[_cType] = newPrice;
     }
 
+    /**
+        @notice change reward apr of one contract type
+        @dev the model of calculating reward requires heavily on reduction percentage. Use reduction percent as
+        the input. Negative percentages are allowed (if want to increase APR). 100_000_000 = 100%.
+        @param _cType contract type to change APR
+        @param reducedPercentage reduction percentage
+    */
     function _changeRewardAPRPerCont(ContType _cType, int256 reducedPercentage) external onlyAuthorities {
         require(reducedPercentage < int256(HUNDRED_PERCENT), "REDUCE_RWD: do not reduce more than 100%");
         aprChangesHistory[_cType].push(
@@ -214,40 +247,37 @@ contract CONTRewardManagement is Initializable {
         aprChangesHistory[_cType].push(APRChangesEntry({ timestamp: initialTstamp, reducedPercentage: 0 }));
     }
 
+    /**
+        @notice change cashout timeout. User cannot claim 2 times in one interval of newTime.
+        @param newTime new length of interval
+    */
     function _changeCashoutTimeout(uint256 newTime) external onlyAuthorities {
         cashoutTimeout = newTime;
     }
 
+    /**
+        @notice change auto APR reduce interval per contract
+        @param newInterval new interval
+    */
     function _changeAutoReduceAPRInterval(uint256 newInterval) external onlyAuthorities {
         autoReduceAPRInterval = newInterval;
     }
 
+    /**
+        @notice change auto APR reduce rate per contract
+        @param newRate new reduction rate (100% == 100_000_000)
+    */
     function _changeAutoReduceAPRRate(uint256 newRate) external onlyAuthorities {
         autoReduceAPRRate = newRate;
     }
 
-    function _updateAdminDashboard() external {
-        uint256 zero = 0;
-        uint256[3] memory breakevenCount = [zero, zero, zero];
-        for (uint64 i = 0; i < contOwners.size(); i++) {
-            address currentAddr = contOwners.getKeyAtIndex(i);
-            for (uint32 j = 0; j < _contsOfUser[currentAddr].length; j++) {
-                ContEntity memory _cont = _contsOfUser[currentAddr][j];
-                uint256 creatime = _cont.creationTime;
-                // breakeven
-                if (_cont.buyPrice <= contRewardInInterval(_cont, creatime, block.timestamp)) {
-                    breakevenCount[uint256(_cont.cType)]++;
-                }
-            }
-        }
-
-        for (uint256 i = 0; i < 3; i++) {
-            emit BreakevenChanged(ContType(i), breakevenCount[i] - _breakevenContsPerType[ContType(i)]);
-            _breakevenContsPerType[ContType(i)] = breakevenCount[i];
-        }
-    }
-
     // ----- External READ functions -----
+    /**
+        @notice calculate initial APR for new contract to display on dApp
+        @dev iterate through a list of APR changes in history
+        @param _cType contract type to query
+        @return result apr of contract type _cType
+    */
     function currentRewardAPRPerNewCont(ContType _cType) external view returns (uint256) {
         uint256 changesLength = aprChangesHistory[_cType].length;
         uint256 result = initRewardAPRPerCont[_cType];
@@ -257,30 +287,35 @@ contract CONTRewardManagement is Initializable {
         return result;
     }
 
+    /**
+        @notice return number of contract of contract type _cType
+        @param _cType contract type to query
+        @return res return number of contract for each contract type
+    */
     function totalContsPerContType(ContType _cType) external view returns (uint256) {
         return _totalContsPerType[_cType];
     }
 
-    function breakevenContsPerType(ContType _cType) external view returns (uint256) {
-        return _breakevenContsPerType[_cType];
-    }
-
-    // function claimedRewardsPerType(ContType _cType) external view returns (uint256) {
-    // return _claimedRewardPerType[_cType];
-    // }
-
-    // function tokensReceivedPerType(ContType _cType) external view returns (uint256) {
-    // return _tokensReceivedPerType[_cType];
-    // }
-
+    /**
+        @notice query if an account is an owner of any contract
+        @param account address to query
+        @return res true if account is the contract number
+    */
     function _isContOwner(address account) external view returns (bool) {
         return isContOwner(account);
     }
 
+    /**
+        @notice query total reward amount of an address in every contract
+        @dev iterate through every contract. Use `contRewardInInterval` to calculate reward in an interval
+        from user last claims to now.
+        @param account address to query
+        @return rewardAmount total amount of reward available for account, tax included
+    */
     function _getRewardAmountOf(address account) external view returns (uint256) {
         if (!isContOwner(account)) return 0;
 
-        uint256 rewardCount = 0;
+        uint256 rewardAmount = 0;
 
         ContEntity[] memory conts = _contsOfUser[account];
         uint256 contsCount = conts.length;
@@ -288,12 +323,20 @@ contract CONTRewardManagement is Initializable {
 
         for (uint256 i = 0; i < contsCount; i++) {
             ContEntity memory _cont = conts[i];
-            rewardCount += contRewardInInterval(_cont, _cont.lastUpdateTime, currentTstamp);
+            rewardAmount += contRewardInInterval(_cont, _cont.lastUpdateTime, currentTstamp);
         }
 
-        return rewardCount;
+        return rewardAmount;
     }
 
+    /**
+        @notice query reward amount of one contract
+        @dev use `contRewardInInterval` to calculate reward in an interval
+        from user last claims to now.
+        @param account address to query
+        @param _contIndex index of contract in user's list
+        @return rewardCont amount of reward available for selected contract
+    */
     function _getRewardAmountOfIndex(address account, uint256 _contIndex) external view returns (uint256) {
         ContEntity[] memory conts = _contsOfUser[account];
         uint256 numberOfConts = conts.length;
@@ -303,6 +346,12 @@ contract CONTRewardManagement is Initializable {
         return rewardCont;
     }
 
+    /**
+        @notice get the list of contracts name from one owner
+        @dev concatenate names into one string, separated by a separator ('#')
+        @param account address to query
+        @return result a string of concatenated result
+    */
     function _getContsNames(address account) external view returns (string memory) {
         if (!isContOwner(account)) return "";
         ContEntity[] memory conts = _contsOfUser[account];
@@ -317,6 +366,12 @@ contract CONTRewardManagement is Initializable {
         return names;
     }
 
+    /**
+        @notice get the list of contracts creation time from one owner
+        @dev concatenate creation time into one string, separated by a separator ('#')
+        @param account address to query
+        @return result a string of concatenated result
+    */
     function _getContsCreationTime(address account) external view returns (string memory) {
         if (!isContOwner(account)) return "";
         ContEntity[] memory conts = _contsOfUser[account];
@@ -332,6 +387,12 @@ contract CONTRewardManagement is Initializable {
         return _creationTimes;
     }
 
+    /**
+        @notice get the list of contracts ctypes from one owner
+        @dev concate ctypes into one string, separated by a separator ('#')
+        @param account address to query
+        @return result a string of concatenated result
+    */
     function _getContsTypes(address account) external view returns (string memory) {
         if (!isContOwner(account)) return "";
         ContEntity[] memory conts = _contsOfUser[account];
@@ -347,6 +408,12 @@ contract CONTRewardManagement is Initializable {
         return _types;
     }
 
+    /**
+        @notice get the list of contracts initial aprs from one owner
+        @dev concate initial aprs into one string, separated by a separator ('#')
+        @param account address to query
+        @return result a string of concatenated result
+    */
     function _getContsInitialAPR(address account) external view returns (string memory) {
         if (!isContOwner(account)) return "";
         ContEntity[] memory conts = _contsOfUser[account];
@@ -362,6 +429,12 @@ contract CONTRewardManagement is Initializable {
         return _types;
     }
 
+    /**
+        @notice get the list of contracts current aprs from one owner
+        @dev concate current aprs into one string, separated by a separator ('#')
+        @param account address to query
+        @return result a string of concatenated result
+    */
     function _getContsCurrentAPR(address account) external view returns (string memory) {
         if (!isContOwner(account)) return "";
 
@@ -378,6 +451,12 @@ contract CONTRewardManagement is Initializable {
         return _types;
     }
 
+    /**
+        @notice get the list of contracts available rewards from one owner
+        @dev concate available rewards into one string, separated by a separator ('#')
+        @param account address to query
+        @return result a string of concatenated result
+    */
     function _getContsRewardAvailable(address account) external view returns (string memory) {
         if (!isContOwner(account)) return "";
         ContEntity[] memory conts = _contsOfUser[account];
@@ -399,6 +478,12 @@ contract CONTRewardManagement is Initializable {
         return _rewardsAvailable;
     }
 
+    /**
+        @notice get the list of contracts last update times from one owner
+        @dev concate last update times into one string, separated by a separator ('#')
+        @param account address to query
+        @return result a string of concatenated result
+    */
     function _getContsLastUpdateTime(address account) external view returns (string memory) {
         if (!isContOwner(account)) return "";
         ContEntity[] memory conts = _contsOfUser[account];
@@ -415,6 +500,11 @@ contract CONTRewardManagement is Initializable {
         return _lastUpdateTimes;
     }
 
+    /**
+        @notice get number of contract from one owner
+        @param account address to query
+        @return count number of contracts owned by this account
+    */
     function _getContNumberOf(address account) public view returns (uint256) {
         return contOwners.get(account);
     }
